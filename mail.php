fcb3b00b0a92d4a3d014f5926363d4cc897c841e
Copper-789e <79974933+Copper-789e@users.noreply.github.com>
2021-07-09 21:41:02 +0200

    update
    - création du formulaire
    - update page "nos réalisations"

---
+++ mail.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Envoi d'un message par formulaire</title>
+</head>
+
+
+<body>
+    <?php
+    $retour = mail('n.gallard129@laposte.net', 'Envoi depuis la page Contact', $_POST['message'], 'From : webmaster@monsite.fr');
+    if ($retour) {
+        echo '<p>Votre message a bien été envoyé.</p>';
+    }
+    ?>
+</body>
+</html>
