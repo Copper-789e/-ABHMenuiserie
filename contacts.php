12c46b0a7dd62901fb7c7b065a568798e5b39114
Copper-789e <79974933+Copper-789e@users.noreply.github.com>
2021-07-19 17:03:45 +0200

    Bug Mobile 1
    - page contact s'affiche mal

--- contacts.php
+++ contacts.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width">
 	<title>ABH Contacts</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" type="text/css" href="contacts.css">
